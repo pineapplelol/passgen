1bf98241acc2e3bbbb98bb1c21198486e5219423
Siraj <sirajchokshi@gmail.com>
2020-12-28 02:29:45 -0500

    🚀 Init commit

---
+++ src/routes/philosophy.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>Philosophy | Passgen</title>
+</svelte:head>
+
+<h1>Philosophy</h1>
+
+<p>This is the 'Philosophy' page. There's not much here.</p>
---
+++ src/routes/privacy.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>Privacy | Passgen</title>
+</svelte:head>
+
+<h1>Privacy</h1>
+
+<p>This is the 'Privacy' page. There's not much here.</p>
