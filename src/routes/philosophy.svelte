dcf2e226d9492504de7ae52207d13a5281071e48
Siraj <sirajchokshi@gmail.com>
2020-12-29 04:58:50 -0500

    style info pages

--- src/routes/philosophy.svelte
+++ src/routes/philosophy.svelte
@@ -2,6 +2,41 @@
   <title>Philosophy | Passgen</title>
 </svelte:head>
 
-<h1>Philosophy</h1>
+<span class="page">
+  <h1>Philosophy</h1>
 
-<p>This is the 'Philosophy' page. There's not much here.</p>
+  <p>
+    Passwords are used multiple times a day, but have you thought out how your
+    password is generated? Do you use a simple password like ‘password’, or do
+    you use a string of random letts and characters that you can barely
+    remember. This platform was inspired by an XKCD, which brings
+  </p>
+
+  <h2>Chances of Hacking</h2>
+
+  <p>
+    Most of the time if your passwords are compromised, its not because your
+    passwords were cracked. Instead, it is because it was sniped off the
+    internet – whether it come from a data breach for a service you use, logging
+    in over Starbucks wifi, or falling prone to a phishing attack. To this
+    degree, the strength of your password is less important than how you keep
+    it. Even if your password can be cracked in 100 tries, services you use will
+    throttle the number of login attempts anyways. That means if a user cannot
+    login after 4 tries, they will suspend anymore login attempts. In this case,
+    the tradeoff between ease of memory and strength of the password can become
+    more balanced.
+  </p>
+
+  <h2>How Is Strength Calculated</h2>
+
+  <p>
+    Calculating entropy. there are 2 ways - look at the whole character set
+    (which is an incorrect way). or we can consider rainbow dictionary attacks.
+    consider the size of the word dictionary set (currently at 7000). From this
+    we calculate we use the scale:
+    <a
+      href="https://twitter.com/Chick3nman512/status/1312529764456914944"
+      target="_blank"
+      rel="noreferrer noopener">https://twitter.com/Chick3nman512/status/1312529764456914944</a>
+  </p>
+</span>
